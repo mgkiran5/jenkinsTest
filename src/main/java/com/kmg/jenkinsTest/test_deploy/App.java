8278a6eb37a3fd19dbccde2d4be3d7394a6e01c9
Kiran MG <mgkiran5@gmail.com>
2026-03-31 16:32:11 -0400

    initial commit

---
+++ src/main/java/com/kmg/jenkinsTest/test_deploy/App.java
@@ -0,0 +1,13 @@
+package com.kmg.jenkinsTest.test_deploy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/kmg/tests/TestCase1.java
@@ -0,0 +1,28 @@
+package com.kmg.tests;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@Test
+	public void print1st() {
+		System.out.println("Printed 1st");
+	}
+
+	@Test
+	public void print2nd() {
+		System.out.println("Printed 2nd");
+	}
+	@Test
+	public void print3rd() {
+		System.out.println("Printed 3rd");
+	}
+	@Test
+	public void print4th() {
+		System.out.println("Printed 4th");
+	}
+	@Test
+	public void print5th() {
+		System.out.println("Printed 5th");
+	}
+}
