0e9f167a3103005311c210b0e899c7da07d369a6
Kiran MG <mgkiran5@gmail.com>
2026-03-31 19:42:57 -0400

    commit after adding assert

--- src/test/java/com/kmg/tests/TestCase1.java
+++ src/test/java/com/kmg/tests/TestCase1.java
@@ -1,5 +1,7 @@
 package com.kmg.tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
 public class TestCase1 {
@@ -7,22 +9,27 @@ public class TestCase1 {
 	@Test
 	public void print1st() {
 		System.out.println("Printed 1st");
+		Assert.assertTrue(false);
 	}
 
 	@Test
 	public void print2nd() {
 		System.out.println("Printed 2nd");
+		Assert.assertTrue(true);
 	}
 	@Test
 	public void print3rd() {
 		System.out.println("Printed 3rd");
+		Assert.assertTrue(false);
 	}
 	@Test
 	public void print4th() {
 		System.out.println("Printed 4th");
+		Assert.assertTrue(true);
 	}
 	@Test
 	public void print5th() {
 		System.out.println("Printed 5th");
+		Assert.assertTrue(true);
 	}
 }
